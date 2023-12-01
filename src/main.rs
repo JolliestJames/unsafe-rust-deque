5be240c7ca6824cf1fbe4a8534c1dc9765dcd056
jame <cloudismydog@gmail.com>
2023-12-01 11:33:03 -0800

    implement structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use std::cmp::Ordering;
+use std::fmt::{self, Debug};
+use std::hash::{Hash, Hasher};
+use std::iter::FromIterator;
+use std::marker::PhantomData;
+use std::ptr::NonNull;
+
+pub struct LinkedList<T> {
+    front: Link<T>,
+    back: Link<T>,
+    len: usize,
+    _boo: PhantomData<T>
+}
+
+type Link<T> = Option<NonNull<Node<T>>>;
+
+struct Node<T> {
+    front: Link<T>,
+    back: Link<T>,
+    elem: T,
+}
+
+pub struct Iter<'a, T> {
+    front: Link<T>,
+    back: Link<T>,
+    len: usize,
+    _boo: PhantomData<T>,
+}
+
+pub struct IterMut<'a, T> {
+    front: Link<T>,
+    back: Link<T>,
+    len: usize,
+    _boo: PhantomData<&'a, mut T>,
+}
+
+pub struct IntoIter<T> {
+    list: LinkedList<t>
+}
+
+pub struct CursorMut<'a, T> {
+    list: &'a mut LinkedList<T>,
+    cursor: Link<T>,
+    index: Option<usize>,
+}
+
 fn main() {
     println!("Hello, world!");
 }
