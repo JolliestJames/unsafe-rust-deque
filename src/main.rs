abaa23a97c54ba0d9822efc897078d0b1cde3fe3
jame <cloudismydog@gmail.com>
2023-12-01 13:14:02 -0800

    implement LinkedList

--- src/main.rs
+++ src/main.rs
@@ -35,13 +35,156 @@ pub struct IterMut<'a, T> {
 }
 
 pub struct IntoIter<T> {
-    list: LinkedList<t>
+    list: LinkedList<T>
 }
 
 pub struct CursorMut<'a, T> {
     list: &'a mut LinkedList<T>,
     cursor: Link<T>,
     index: Option<usize>,
+}
+
+impl<T> LinkedList<T> {
+    pub fn new() -> Self {
+        Self {
+            front: None,
+            back: None,
+            len: 0,
+            _boo: PhantomData,
+        }
+    }
+
+    pub fn push_front(&mut self, elem: T) {
+        unsafe {
+            let new = NonNull::new_unchecked(Box::into_raw(Box::new(Node {
+                front: None,
+                back: None,
+                elem,
+            })));
+
+            if let Some(old) = self.front {
+                (*old.as_ptr()).front = Some(new);
+                (*new.as_ptr()).back = Some(old);
+            } else {
+                self.back = Some(new);
+            }
+
+            self.front = Some(new);
+            self.len += 1;
+        }
+    }
+
+    pub fn push_back(&mut self, elem: T) {
+        unsafe {
+            let new = NonNull::new_unchecked(Box::into_raw(Box::new(Node {
+                front: None,
+                back: None,
+                elem,
+            })));
+
+            if let Some(old) = self.back {
+                (*old.as_ptr()).back = Some(new);
+                (*new.as_ptr()).front = Some(old);
+            } else {
+                self.front = Some(new)
+            }
+
+            self.back = Some(new);
+            self.len += 1;
+        }
+    }
+
+    pub fn pop_front(&mut self) -> Option<T> {
+        unsafe {
+            self.front.map(|node| {
+                let boxed_node = Box::from_raw(node.as_ptr());
+                let result = boxed_node.elem;
+
+                self.front = boxed_node.back;
+                if let Some(new) = self.front {
+                    (*new.as_ptr()).front = None;
+                } else {
+                    self.back = None;
+                }
+
+                self.len -= 1;
+                result
+            })
+        }
+    }
+
+    pub fn pop_back(&mut self) -> Option<T> {
+        unsafe {
+            self.back.map(|node| {
+                let boxed_node = Box::from_raw(node.as_ptr());
+                let result = boxed_node.elem;
+
+                self.back = boxed_node.front;
+                if let Some(new) = self.back {
+                    (*new.as_ptr()).back = None;
+                } else {
+                    self.front = None;
+                }
+
+                self.len -= 1;
+                result
+            })
+        }
+    }
+
+    pub fn front(&self) -> Option<&T> {
+        unsafe { self.front.map(|node| &(*node.as_ptr()).elem) }
+    }
+
+    pub fn front_mut(&mut self) -> Option<&T> {
+        unsafe { self.front.map(|node| &mut (*node.as_ptr()).elem) }
+    }
+
+    pub fn back(&self) -> Option<&T> {
+        unsafe { self.back.map(|node| &(*node.as_ptr()).elem) }
+    }
+
+    pub fn back_mut(&self) -> Option<&T> {
+        unsafe { self.back.map(|node| &mut (*node.as_ptr()).elem) }
+    }
+
+    pub fn len(&self) -> usize {
+        self.len
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.len == 0
+    }
+
+    pub fn clear(&mut self) {
+        while self.pop_front().is_some() {}
+    }
+
+    pub fn iter(&self) -> Iter<T> {
+        Iter {
+            front: self.front,
+            back: self.back,
+            len: self.len,
+            _boo: PhantomData,
+        }
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<T> {
+        Iter {
+            front: self.front,
+            back: self.back,
+            len: self.len,
+            _boo: PhantomData,
+        }
+    }
+
+    pub fn cursor_mut(&mut self) -> CursorMut<T> {
+        CursorMut {
+            list: self,
+            cursor: None,
+            index: None,
+        }
+    }
 }
 
 fn main() {
